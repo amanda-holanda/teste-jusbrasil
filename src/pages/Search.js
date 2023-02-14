68b70f4edfe0b56e825cf20ec6607d1fce6c3848
Amanda Holanda <amandaholanda_@hotmail.com>
2023-02-14 17:44:33 -0300

    estilização inicial

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,9 +2,20 @@ import './Search.css';
 
 function Search() {
   return (
-    <div className="Search">
-      <h1>Jusbrasil</h1>
-    </div>
+    <section className='searchContainer'>
+      <h1 className='searchTitle'>Buscar</h1>
+      <p className='searchParagraph'>Selecione um tribunal para listar os processos ou buscar pelo número unificado</p>
+      <form className='searchForm'>
+        <label>
+          <input type='number' />
+        </label>
+        <label>
+          <input type='number' placeholder='Número do processo'/>
+        </label>
+        <input type='submit' value='Buscar' />
+
+      </form>
+    </section>
   );
 }
 
