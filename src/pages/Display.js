7066595d6c05c0649ef42f9181f7fa662475f967
Amanda Holanda <amandaholanda_@hotmail.com>
2023-02-23 18:20:29 -0300

    estilizacao do botao de return

--- src/pages/Display.js
+++ src/pages/Display.js
@@ -2,13 +2,15 @@ import ProcessList from "../components/processList/ProcessList";
 import { filterData } from "../services/filterProcess";
 import { fetchData } from "../services/fetchData";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Display.css";
 
 export const Display = () => {
   const [listFiltred, setListFiltred] = useState([]);
   const urlParameter = window.location.search;
   const searchParams = new URLSearchParams(urlParameter);
   const processNumber = searchParams.get("numeroProcesso");
-  
+
   const loadData = async (processNumber) => {
     const data = await fetchData("db.json");
     const dataFiltred = filterData(data, processNumber);
@@ -16,7 +18,10 @@ export const Display = () => {
   };
 
   return (
-    <>      
+    <>
+      <Link className="returnButton" to="/">
+        Voltar
+      </Link>
       <ProcessList onDisplay={loadData(processNumber)} list={listFiltred} />
     </>
   );
